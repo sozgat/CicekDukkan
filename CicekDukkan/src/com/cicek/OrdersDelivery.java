e0496cdf78f7c0cf9c4dfb616aeb64a54fd7e48e
Cihan Koç <cihankoc41@gmail.com>
2019-03-04 18:38:37 +0300

    said

--- CicekDukkan/src/com/cicek/OrdersDelivery.java
+++ CicekDukkan/src/com/cicek/OrdersDelivery.java
@@ -1,6 +1,10 @@
 package com.cicek;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import com.model.*;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import com.utility.ExcelRead;
@@ -76,9 +80,9 @@ public class OrdersDelivery {
 	public static void testDistance(ArrayList<DistanceCompany> distanceArray)
 	{
 		double redTemp, greenTemp, blueTemp;
-		ArrayList<String> red	 = new ArrayList<String>();
-		ArrayList<String> green	 = new ArrayList<String>();
-		ArrayList<String> blue	 = new ArrayList<String>();
+		ArrayList<Double> red	 = new ArrayList<Double>();
+		ArrayList<Double> green	 = new ArrayList<Double>();
+		ArrayList<Double> blue	 = new ArrayList<Double>();
 		
 		
 		for(int i=0; i<distanceArray.size(); i++)
@@ -87,7 +91,7 @@ public class OrdersDelivery {
 			greenTemp = distanceArray.get(i).getGreenDistance();
 			blueTemp = distanceArray.get(i).getBlueDistance();
 
-			int j = 0 ;
+
 			// Temel Bölge = Kýrmýzý
 			if( distanceArray.get(i).getRedDistance() < distanceArray.get(i).getGreenDistance() && distanceArray.get(i).getRedDistance() < distanceArray.get(i).getBlueDistance() ) {
 				
@@ -95,15 +99,15 @@ public class OrdersDelivery {
 				
 				if ( red.size() < 30 ) {	
 					
-					red.add("Sipariþ ");
+					red.add(redTemp);
 					
 				} else if ( distanceArray.get(i).getGreenDistance() < distanceArray.get(i).getBlueDistance() && green.size() < 50 ){
 					
-					green.add("Sipariþ ");
+					green.add(greenTemp);
 					
 				} else if ( blue.size() < 35 ) {
 					
-					blue.add("Sipariþ");
+					blue.add(blueTemp);
 					
 				} else {
 					
@@ -117,15 +121,15 @@ public class OrdersDelivery {
 				
 				if ( green.size() < 50) {
 					
-					green.add("Sipariþ ");
+					green.add(greenTemp);
 					
 				} else if ( distanceArray.get(i).getRedDistance() < distanceArray.get(i).getBlueDistance() && red.size() <30) {
 					
-					red.add("Sipariþ ");
+					red.add(redTemp);
 					
 				} else if (blue.size() < 30 ) {
 					
-					blue.add("Sipariþ");
+					blue.add(blueTemp);
 					
 				} else {					
 					System.out.println("Kayýt Yapýlamadý - Yeþil Bölge");	
@@ -139,15 +143,15 @@ public class OrdersDelivery {
 				// max 80 fakat minimum deðerleri saðlamasý icin 45'den fazla olamaz
 				if ( blue.size() < 45 ) { 
 					
-					blue.add("Sipariþ ");
+					blue.add(blueTemp);
 				
 				} else if ( red.size() < 20 ) {	
 					
-					red.add("Sipariþ ");
+					red.add(redTemp);
 					
 				} else if ( green.size() < 35 ) {
 					
-					green.add("Sipariþ");
+					green.add(greenTemp);
 					
 				} else {
 					System.out.println("Kayýt Yapýlamadý - Mavi Bölge");
@@ -164,18 +168,45 @@ public class OrdersDelivery {
 			System.out.println("{" + redTemp + " , " + greenTemp + " , " + blueTemp + "}");
 		}
 		
-		
-		
+
 		
 		// Hangi Þubede Kaç Sipariþ Var?
 		System.out.println("\nToplam Sipariþ (Kýrmýzý)	: " + red.size());
 		System.out.println("Toplam Sipariþ (Yeþil)   	: " + green.size());
 		System.out.println("Toplam Sipariþ (Mavi)	 	: " + blue.size());
-	
+		System.out.println("\n");
+		
+		//  Sorting of arraylist using Collections.sort*/
+		 Collections.sort(red);
+		 Collections.sort(green);
+		 Collections.sort(blue);
+		
+		
+//		for( int i = 0; i < green.size(); i++  ) {
+//			System.out.println( i + ".  Eleman : " + green.get(i));
+//		}
+		
+		System.out.println("\n Kýrmýzýlar : \n");
+		int k = 0;
+		for(double counter: red ) {
+			System.out.println( k++ + ".  Kýrmýzý : " + counter);
+		}
+		
+		System.out.println("\n Yeþiller : \n");
+		k = 0;
+		for(double counter: green ) {
+			System.out.println( k++ + ".  Yeþil : " + counter);
+		}
+		
+		System.out.println("\n Maviler : \n");
+		k = 0;
+		for(double counter: blue ) {
+			System.out.println( k++ + ".  Mavi : " + counter);
+		}
+		
+		
 		
 	
-		
-		
 		
 	}
 }
